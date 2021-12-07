46efc541210334320970c72620163a41a10b45d5
cameron <cameron.studdstreet@gmail.com>
2021-12-07 05:22:27 +0000

    day7

---
+++ src/day7.rs
@@ -0,0 +1,76 @@
+use std::ops::RangeInclusive;
+
+use crate::input_const;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Crabs(Vec<i128>);
+
+impl Crabs {
+    fn get_range(&self) -> RangeInclusive<i128> {
+        let min = *self.0.iter().min().unwrap();
+        let max = *self.0.iter().max().unwrap();
+        min..=max
+    }
+
+    fn total_distance(&self, x: i128, q2: bool) -> i128 {
+        self.0.iter().map(|s| (s - x).abs()).map(distance_fn(q2)).sum()
+    }
+}
+
+fn distance_fn(q2: bool) -> fn(i128) -> i128 {
+    if q2 {
+        triangle
+    } else {
+        id
+    }
+}
+fn triangle(x: i128) -> i128 {
+    x * (x + 1) / 2
+}
+
+fn id(x: i128) -> i128 {
+    x
+}
+
+pub fn solution1() -> i128 {
+    calculate(input_const!("7"), false)
+}
+
+pub fn solution2() -> i128 {
+    calculate(input_const!("7"), false)
+}
+
+fn calculate(s: impl AsRef<str>, q2: bool) -> i128 {
+    let crabs = parse_input(s);
+    crabs
+        .get_range()
+        .map(|x| crabs.total_distance(x, q2))
+        .min()
+        .unwrap()
+}
+
+fn parse_input(s: impl AsRef<str>) -> Crabs {
+    let nums = s.as_ref().split(",").map(|s| s.trim().parse().unwrap());
+    Crabs(nums.collect())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const GIVEN_INPUT: &str = "16,1,2,0,4,2,7,1,2,14";
+
+    #[test]
+    fn parses_input() {
+        assert_eq!(
+            parse_input(GIVEN_INPUT),
+            Crabs(vec![16, 1, 2, 0, 4, 2, 7, 1, 2, 14])
+        )
+    }
+
+    #[test]
+    fn given_example() {
+        assert_eq!(calculate(GIVEN_INPUT, false), 37);
+        assert_eq!(calculate(GIVEN_INPUT, true), 168);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,16 @@
 
 extern crate test;
 
-mod utils;
 mod day1;
 mod day2;
 mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
+mod utils;
 
 fn main() {
-    println!("{}", day6::solution2());
+    dbg!(day7::solution1());
+    dbg!(day7::solution2());
 }
-
-
